283a004119afbdf1c5f7593a82215568e1796a71
ttothewiggy <76453825+ttothewiggy@users.noreply.github.com>
2021-08-24 22:22:58 +1200

    Portfolio

---
+++ Creative Studio 1708/myscript.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// Inside the Media Query.
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
